f99b6c0dd00b5bda2ed938d973bcc962ecc91f66
maniarasan2k <maniarasan2k@gmail.com>
2025-11-24 17:16:15 +0530

    client updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import HTML from "./pages/HTML";
-import CSS from "./pages/CSS";
+import HTML from "./pages/Html";
+import CSS from "./pages/Css";
 import JS from "./pages/JS";
 import About from "./pages/About";
 
